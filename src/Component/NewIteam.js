517f426fc905facf8c15b8a411b69bb6e3e70153
Parth <84613656+parthjadhao@users.noreply.github.com>
2023-11-15 14:44:24 +0530

    video-24:fetch api keys

--- src/Component/NewIteam.js
+++ src/Component/NewIteam.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewIteam extends Component {
   render() {
+    let { title, description } = this.props;
     return (
-      <div>
-        this is news iteam 
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/1A18/production/_131708660_gettyimages-490665558.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/newdetails" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewIteam
+export default NewIteam;
--- src/Component/News.js
+++ src/Component/News.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react'
-import NewIteam from './NewIteam'
+import React, { Component } from "react";
+import NewIteam from "./NewIteam";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is news components
-        <NewIteam/>
-        <NewIteam/>
-        <NewIteam/>
-        <NewIteam/>
-        <NewIteam/>
+      <div className="container my-3">
+        <h2>NewsMonkey top-headline</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewIteam title="my title" description="this is my description" />
+          </div>
+          <div className="col-md-4">
+            <NewIteam title="my title" description="this is my description" />
+          </div>
+          <div className="col-md-4">
+            <NewIteam title="my title" description="this is my description" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
