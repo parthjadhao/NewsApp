24fd28e6db9bb24c8a9627dd9b0b0201d3c6e5df
Parth <84613656+parthjadhao@users.noreply.github.com>
2023-12-12 19:04:08 +0530

    video:39-converted class based component into function based components

--- src/component/NewIteam.jsx
+++ src/component/NewIteam.jsx
@@ -1,42 +1,40 @@
 import React, { Component } from "react";
 
-export class NewIteam extends Component {
-  render() {
-    // props in class based react component
-    let { title, description, urlToImage, url, author, date ,source } = this.props;
+const NewIteam = (props) => {
+  // props in class based react component
+  let { title, description, urlToImage, url, author, date, source } =
+    props;
 
-    return (
-      <div className="my-3">
-        <div className="card">
-          <img
-            src={
-              !urlToImage
-                ? "https://www.racefans.net/wp-content/uploads/2023/11/racefansdotnet-7089981_HiRes.jpg"
-                : urlToImage
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <span className="position-absolute top-0 start-90 translate-middle badge rounded-pill bg-danger">
-              {source}
-            </span>
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                by {author ? author : "unknown"} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a href={url} className="btn btn-primary">
-              Read More
-            </a>
-          </div>
+  return (
+    <div className="my-3">
+      <div className="card">
+        <img
+          src={
+            !urlToImage
+              ? "https://www.racefans.net/wp-content/uploads/2023/11/racefansdotnet-7089981_HiRes.jpg"
+              : urlToImage
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <span className="position-absolute top-0 start-90 translate-middle badge rounded-pill bg-danger">
+            {source}
+          </span>
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              by {author ? author : "unknown"} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a href={url} className="btn btn-primary">
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewIteam;
--- src/component/News.jsx
+++ src/component/News.jsx
@@ -1,109 +1,98 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import NewIteam from "./NewIteam";
 import Spinner from "./spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
-  };
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      page: 1,
-      loading: true,
-      totalResults: 0,
-    };
-    document.title = `${this.props.category}-NewsMonkey`;
-    // document.title = this.props.category;
-  }
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+const News = (props) => {
+  // declared states
+  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [totalResults, setTotalResults] = useState(0);
+  // document.title = `${props.category}-NewsMonkey`;
+
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parseData.articles),
-      totalResults: parseData.totalResults,
-      loading: false,
-    });
+    setArticles(parseData.articles);
+    setTotalResults(parseData.totalResults);
+    setLoading(false);
   };
-  async newUpdate() {
-    this.props.setProgress(30);
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  const newUpdate = async () => {
+    props.setProgress(30);
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
     let data = await fetch(url);
-    this.props.setProgress(50);
+    props.setProgress(50);
     let parseData = await data.json();
-    this.props.setProgress(80);
-    this.setState({
-      articles: parseData.articles,
-      totalResults: parseData.totalResults,
-      loading: false,
-    });
-    this.props.setProgress(100);
-  }
-
-  async componentDidMount() {
-    this.newUpdate();
-  }
-
-  handlePrevClick = async () => {
-    this.setState({ page: this.state.page - 1 });
-    this.newUpdate();
+    props.setProgress(80);
+    setArticles(parseData.articles);
+    setTotalResults(parseData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
   };
-  handleNextClick = async () => {
-    this.setState({ page: this.state.page + 1 });
-    this.newUpdate();
+
+  useEffect(()=>{
+    newUpdate()
+  },[])
+  
+
+  const handlePrevClick = async () => {
+    setPage(page - 1);
+    newUpdate();
   };
-  render() {
-    console.log("rendering");
-    return (
-      <div className="container">
-        <h2>NewsMonkey top {this.props.category} headline</h2>
-        {this.state.loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner />}
-          endMessage={
-            <p style={{ textAlign: "center" }}>
-              <b>--x--</b>
-            </p>
-          }
-        >
-          <div className="container">
-            <div className="row">
-              {this.state.articles.map((element) => {
-                return (
-                  <div className="col-md-4">
-                    <NewIteam
-                      title={element.title}
-                      description={element.description}
-                      urlToImage={element.urlToImage}
-                      url={element.url}
-                      author={element.author}
-                      date={element.publishedAt}
-                      source={element.source.name}
-                    />
-                  </div>
-                );
-              })}
-            </div>
+  const handleNextClick = async () => {
+    setPage(page + 1);
+    newUpdate();
+  };
+  return (
+    <div className="container">
+      <h2>NewsMonkey top {props.category} headline</h2>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>--x--</b>
+          </p>
+        }
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
+              return (
+                <div className="col-md-4">
+                  <NewIteam
+                    title={element.title}
+                    description={element.description}
+                    urlToImage={element.urlToImage}
+                    url={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
           </div>
-        </InfiniteScroll>
-      </div>
-    );
-  }
-}
-
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 export default News;
